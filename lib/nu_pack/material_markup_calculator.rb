227e494dcfc98185f0cafcb14dd9e481e65c0595
Waseem Ahmad <talk.to.waseem@gmail.com>
2016-12-25 07:45:57 +0530

    Working MaterialMarkupCalculator.

--- lib/nu_pack/material_markup_calculator.rb
+++ lib/nu_pack/material_markup_calculator.rb
@@ -5,6 +5,13 @@ module NuPack
     end
 
     def markup
+      case @material
+      when 'food'        then 0.13
+      when 'electronics' then 0.02
+      when 'drugs'       then 0.075
+      else
+        0
+      end
     end
   end
 end
